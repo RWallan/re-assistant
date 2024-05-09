cde37e65ad365cb6f5a70cdc1d5a697f79779f8a
RWallan <3am.richardwallan@gmail.com>
2024-05-09 15:35:07 -0300

    feat: gerencia as configurações do scraping

---
+++ re_assistant/collect/config.py
@@ -0,0 +1,54 @@
+"""
+Este módulo é responsável por configurar os requests para o scraping.
+"""
+
+from pathlib import Path
+from typing import Type
+
+from pydantic import BaseModel
+from pydantic_settings import (
+    BaseSettings,
+    PydanticBaseSettingsSource,
+    SettingsConfigDict,
+    TomlConfigSettingsSource,
+)
+
+HERE = Path(__file__).parent
+
+
+class Headers(BaseModel):
+    accept: str
+    accept_language: str
+    cache_control: str
+    priority: str
+    referer: str
+    sec_ch_ua: str
+    sec_ch_ua_mobile: str
+    sec_ch_ua_platform: str
+    sec_fetch_dest: str
+    sec_fetch_mode: str
+    sec_fetch_site: str
+    sec_fetch_user: str
+    sec_gpc: str
+    upgrade_insecure_requests: str
+    user_agent: str
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(toml_file=HERE / 'config.toml')
+    catalog_url: str
+    headers: Headers
+
+    @classmethod
+    def settings_customise_sources(  # noqa
+        cls,
+        settings_cls: Type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        return (TomlConfigSettingsSource(settings_cls),)
+
+
+settings = Settings()  # pyright: ignore
